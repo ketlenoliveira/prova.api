1deda750d4e2bc9c3ebd0d4c248d57f01654efed
ketlen <ketlenoliveira753@gmail.com>
2022-06-01 11:40:39 -0300

    novo commit

---
+++ api/index.js
@@ -0,0 +1,16 @@
+import 'dotenv/config'
+import { con } from '  ./repository/connection.js'
+import express from 'express'
+import cors from 'cors'
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+
+
+
+
+
+
+server.listen(process.env.PORT,() => console.log (`API Conectada na porta ${process.env.PORT}`));
